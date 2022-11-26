b2b5576141312df1f0565e622d21726d47a92445
Enjoy3325 <enjoy3325@gmail.com>
2022-11-26 18:52:07 +0200

    first commit

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,5 +1,5 @@
-// import PropTypes from 'prop-types';
 import { ItemGalleryLi, ImageGalleryImg } from './ImageGalleryItem.styled';
+// import PropTypes from 'prop-types';
 import React from 'react';
 
 export const ImageGalleryItem = ({ tags, webformatURL }) => {
